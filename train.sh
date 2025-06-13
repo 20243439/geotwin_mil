a06faff574fdac6d5cfd600b289ebacb58d8f64a
Chanjae Song <134582367+20243439@users.noreply.github.com>
2025-06-13 23:59:05 +0900

    Update train.sh
    for main classification with geotwin features

--- train.sh
+++ train.sh
@@ -1,34 +1,17 @@
-python main.py --model_name resnet50 --output_dim 22 --feature_type map --mode few --shot 1
-
-python main.py --model_name vit_b_16
-
-python main.py --model_name swin_v2_t
-
-python main.py --model_name simclr 
-
-python main.py --model_name min
-
-python main.py --model_name mean 
-
-python main.py --model_name max
-
-python main.py --model_name hadamard
-
-python main.py --model_name conc
-
-python main.py --model_name gated
-
-python main.py --model_name cross
-
-python main.py --model_name moco
-
-python warm.py --model_name moco --output_dim 22 --train_group small --valid_group small --test_group small --seed 1
-
-python cold.py \
-  --image_dir "./beijing_image/image" \
-  --label_dir "./beijing_image/label" \
-  --checkpoint best_resnet_func_1.pth \
-  --output_json bj_resnet_func_1.json \
-  --model_name resnet50 \
+python main.py \
+  --mode few \
+  --shot 16  \
+  --feature_type image \
+  --model_name geotwin \
+  --label_dir "./seoul_image/label" \
+  --batch_size 32 \
   --output_dim 6 \
-  --patch_size 224
+  --lr 1e-3 \
+  --weight_decay 1e-5 \
+  --num_epochs 50 \
+  --early stop patience 10 \
+  --seed 42
+
+  
+  
+  
